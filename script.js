3e9bf5e17321a40a8517ab44f29ebde8d9124878
gtrajanoo <gabrieltrajanof@gmail.com>
2022-07-14 21:25:20 -0300

    Finalizando o projeto

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const loginCadastrado = "tryber@teste.com";
-const senhaCadastrada = "123456";
-
-const loginInformado = document.getElementById("email");
-const senhaInformada = document.getElementById("senha");
-const buttonLogin = document.getElementById("button");
+const loginCadastrado = 'tryber@teste.com';
+const senhaCadastrada = '123456';
+const loginInformado = document.getElementById('email');
+const senhaInformada = document.getElementById('senha');
+const buttonLogin = document.getElementById('button');
 
 function verificaLogin() {
   if (loginInformado.value === loginCadastrado && senhaInformada.value === senhaCadastrada) {
-    alert("Olá, Tryber!");
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
 }
-buttonLogin.addEventListener("click", verificaLogin); 
+buttonLogin.addEventListener('click', verificaLogin);
 
+const button = document.getElementById('submit-btn');
+const checkbox = document.getElementById('agreement');
+checkbox.addEventListener('click', (e) => {
+  if (e.target.checked) {
+    button.removeAttribute('disabled');
+  } else {
+    button.disabled = true;
+  }
+});
